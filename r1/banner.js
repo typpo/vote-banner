1b88c34e01afe3bcdef6ba5feabd23245fe56fb6
Ian Webster <ianw_github@ianww.com>
2018-10-10 15:15:49 -0700

    Set cookie when vote banner is hidden

--- r1/banner.js
+++ r1/banner.js
@@ -1,4 +1,9 @@
 (function() {
+  var COOKIE_NAME = 'vote-banner__hide';
+  if (readCookie(COOKIE_NAME)) {
+    return;
+  }
+
   var DEFAULT_CONFIG = {
     backgroundColor: '#f26522',
     fontColor: '#000',
@@ -126,15 +131,21 @@
     // Use this instead of an inline to avoid running into unsafe-inline
     // content security policies.
     document.getElementById('vote-banner__close').onclick = function() {
-      window.closeVoteBanner();
+      window.__voteBanner__close();
     }
   }
 
-  window.closeVoteBanner = function() {
+  window.voteBanner__close = function() {
     var elt = document.getElementById('vote-banner');
     elt.parentNode.removeChild(elt);
     adjustPositionFixed(true /* reverse */);
     document.getElementById('vote-banner__wrap').style.marginTop = 0;
+
+    createCookie(COOKIE_NAME, 1, 7);
+  }
+
+  window.voteBanner__resetCookie = function() {
+    createCookie(COOKIE_NAME, '', -1);
   }
 
   function addBanner() {
@@ -144,6 +155,36 @@
 <a id='vote-banner__lookup' class='vote-banner__button' href='https://bit.ly/2Cb2FS2' target='_blank'>Get Polling Location</a> \
 <div id='vote-banner__close' class='vote-banner__close'>&times;</div></div> \
 </div>");
+  }
+
+  // Create cookie
+  function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      expires = "; expires="+date.toGMTString();
+    }
+    else {
+      expires = "";
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  // Read cookie
+  function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1,c.length);
+      }
+      if (c.indexOf(nameEQ) === 0) {
+        return c.substring(nameEQ.length,c.length);
+      }
+    }
+    return null;
   }
 
   injectStyles();
